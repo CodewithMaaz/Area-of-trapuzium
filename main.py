d65b70685e71fec79b066253904e61d1f3af5b82
Maaz Ahmed <93466155+CodewithMaaz@users.noreply.github.com>
2022-01-06 13:47:16 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+'''Made by code with maaz4
+
+area of trapezium'''
+print("  Calculator trapezium")
+print("pls sub bye")
+base_1 = input("Enter base 1 ")
+base_2 = input("Enter base 2 ")
+Height = input("Enter Height ")
+base_1 = int(base_1)
+base_2 = int(base_2)
+Height = int(Height)
+
+area = ((base_1 + base_2) / 2) * Height
+
+print(area)
